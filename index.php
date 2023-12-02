7af33d4174df6fba921c22c688eac87019a31ec6
Jair_Toxic <alexis151270sanchez@gmail.com>
2023-12-01 19:08:37 -0500

    Registro de usuario al hotel v1.0

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
     <title>Registrar usuario</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <link rel="stylesheet" type="text/css" href="estilos/estilo.css">
 </head>
 <body>
     <div class="login-box">
-        <form method="post" action="validar_login.php">
+        <form method="post" action="registrar.php">
             <div class="user-box">
                 <input type="text" name="name" placeholder="Nombre" required>
             </div>
@@ -21,7 +21,7 @@
     </div>
 
     <?php
-    include("validar_login.php");
+    include("registrar.php");
     ?>
 </body>
 </html>
